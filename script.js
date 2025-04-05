46290eb8c4746b51ef5348e9c2847690dfa0a52b
Segun Adesokan <142250556+sheezylion@users.noreply.github.com>
2025-04-05 16:53:08 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-fetch("http://localhost:5000/hello")
-  .then((res) => res.json())
-  .then((data) => {
-    document.getElementById("backend-message").textContent = data.message;
-  })
-  .catch((err) => {
-    document.getElementById("backend-message").textContent =
-      "Failed to fetch from backend.";
-  });
